eb1793c6986f1854138df2bc5e282e8016b0d499
ayame <ayame2117@gmail.com>
2024-12-29 16:44:13 +0900

    カテゴリー選択したら、データベースに保存する処理と、画面遷移の処理を追加した。

--- category.py
+++ category.py
@@ -12,28 +12,28 @@ class Data(db.Model):
     category = db.Column(db.String(50))
     text = db.Column(db.Text)
 
-#ボタンで選択したカテゴリーをデータベースに保存する。合ってるか不安なところ
-@app.post('/debate_page')
-def buttun_category():
-    category = request.form["category"]#name=categoryのボタンを選択したら、categoryとして保存される。
-      # データベースに保存
-    new_category = Category(category=category)#データベースのcategory列に追加
-    db.session.add(new_category)
-    db.session.commit()
 
 
-
-#自分で書いたお題をデータベースに保存する。プロンプトに渡す
+#選んだカテゴリーと自分で書いたお題をデータベースに保存する。
 @app.post('/debate_page')
 def submit_topic():
-    # フォームに書いたテキスト値を取得
-    #categoryなしor topicと同じにする
+    category = request.form["category"]#name=categoryのボタンを選択したら、categoryとして保存される。
     text = request.form["free_text"]
-    # データベースに保存
-    new_topic = Topic(text=text)
-    db.session.add(new_topic)
+
+    if category:
+        # カテゴリーの保存処理
+        new_category = Category(category=category)
+        db.session.add(new_category)
+    if text:
+        # 話題の保存処理
+        new_topic = Topic(text=text)
+        db.session.add(new_topic)
     db.session.commit()
 
+
+#categoryを選択したら、その場でAIが議題を生成する画面にいくか、議論画面で議題を作るか
+
+#画面遷移
 @app.get("/next_page5")#ボタンを押したら次のページに行く、議論開始！という画面を挟んで、ディベート画面へ遷移
 def next_page5():
     return render_template("debate_start.html")#("debate.html")それか、そのままチャット画面に遷移するか
